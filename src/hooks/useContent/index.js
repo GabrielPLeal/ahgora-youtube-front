dad373dc32a8d802d86c77c11e6d9859b8b146a4
Gabriel Leal <gabriel.leal@advicehealth.com.br>
2023-01-30 23:50:01 -0300

    feature: Implemented yup validation

--- src/hooks/useContent/index.js
+++ src/hooks/useContent/index.js
@@ -1,9 +1,16 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { weekDayYup } from '../../utils';
 
 const useContent = () => {
-    const { register, setValue, getValues, formState: { errors }, handleSubmit } = useForm()
+    const { register, setValue, getValues, formState: { errors }, handleSubmit } = useForm({
+        resolver: yupResolver(validationSchema)
+    })
 
-    const submit = (data) => { }
+    const submit = (data) => {
+    }
+
 
     return {
         register,
@@ -14,5 +21,16 @@ const useContent = () => {
         submit
     }
 }
+
+const validationSchema = yup.object().shape({
+    search: yup.string().required('Campo obrigatório!').nullable(),
+    mon: weekDayYup(),
+    tues: weekDayYup(),
+    wed: weekDayYup(),
+    thurs: weekDayYup(),
+    fri: weekDayYup(),
+    sat: weekDayYup(),
+    sun: weekDayYup(),
+})
 
 export default useContent
